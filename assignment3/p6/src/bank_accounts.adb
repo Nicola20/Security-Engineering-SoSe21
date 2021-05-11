49f839b36116a2b1711036f9dd55b478fdb25755
Philipp Tornow <philipp_oeffentlich@hotmail.de>
2021-05-11 15:04:17 +0200

    started task 6

---
+++ assignment3/p6/src/bank_accounts.adb
@@ -0,0 +1,36 @@
+    with Ada.Text_IO;
+    with Bank_Accounts; use Bank_Accounts;
+
+package body Bank_Accounts is
+
+    package TEXTIO renames Ada.Text_IO;
+
+    function Get_Balance(Account: Account_Type) return Cents_Type is
+    begin
+        return  Account.Balance;
+    end Get_Balance;
+
+    procedure Deposit(Account: in out Account_Type; Amount: Cents_Type) is
+    begin
+        Account.Balance := Account.Balance + Amount;
+    end Deposit;
+
+    procedure Withdraw(Account: in out Account_Type; Amount: Cents_Type) is
+    begin
+        Account.Balance := Account.Balance - Amount;
+    end Withdraw;
+
+    procedure Transfer(From: in out Account_Type; To: in out Account_Type; Amount: Cents_Type) is
+    begin
+        Withdraw(From, Amount);
+        Deposit(To, Amount);
+    end Transfer;
+
+    procedure Put(Account: in Account_Type) is
+    begin
+        TEXTIO.Put("Current Balance: ");
+        TEXTIO.Put(Account.Get_Balance(Account)'Image);
+        TEXTIO.Put(" Euro");
+    end Put;
+
+end Bank_Accounts;
--- assignment3/p6/src/bank_accounts.ads
+++ assignment3/p6/src/bank_accounts.ads
@@ -7,13 +7,22 @@ package Bank_Accounts is
     function Get_Balance(Account: Account_Type) return Cents_Type;
     -- Returns the current Balance from Account.
 
-    procedure Deposit(Account: in out Account_Type; Amount: Cents_Type);
+    procedure Deposit(Account: in out Account_Type; Amount: Cents_Type)
+    with
+        Pre =>(Amount > 0) and (Get_Balance(Account)+Amount < Integer'Last),
+        Post =>(Get_Balance(Account) = Get_Balance(Account)'Old + Amount);
 
-    procedure Withdraw(Account: in out Account_Type; Amount: Cents_Type);
+    procedure Withdraw(Account: in out Account_Type; Amount: Cents_Type)
+    with
+        Pre =>(Get_Balance(Account) > 0) and (Amount <= Get_Balance(Account)),
+        Post =>(Get_Balance(Account) >= 0) and (Get_Balance(Account) = Get_Balance(Account)'Old - Amount);
 
     procedure Transfer(From: in out Account_Type;
                        To: in out Account_Type;
-                       Amount: Cents_Type);
+                       Amount: Cents_Type)
+    with
+        Pre =>(Amount > 0) and (Get_Balance(From) >= Amount),
+        Post =>(Get_Balance(From) = Get_Balance(From)'Old - Amount) and (Get_Balance(To) = Get_Balance(To)'Old + Amount);
 
     procedure Put(Account: in Account_Type);
     -- Prints the current Balance of the given Account.
