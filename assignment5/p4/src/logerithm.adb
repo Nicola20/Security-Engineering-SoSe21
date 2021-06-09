fa6f5dcfc7aef8bb94a8f37fb31a85a5d8951b7f
Montag55 <temeter@yahoo.com>
2021-06-09 07:21:20 +0200

    added task 4, post still fucked though

---
+++ assignment5/p4/src/logerithm.adb
@@ -0,0 +1,44 @@
+with Logerithm;
+
+package body Logerithm with
+   SPARK_Mode => On
+is
+
+   function Log(Base, Value : Integer) return Integer is
+      Log : Natural := 0;
+      Pow : Positive := 1;
+   begin
+      if Value = 1 then
+         pragma Assert(Base**Log = Pow);
+         return Log;
+      end if;
+
+
+      while Pow <= (Value / Base)
+        and Log < Integer'Last
+        and Pow <= Integer'Last / Base
+        and Base <= Integer'Last / Pow
+      loop
+         pragma Loop_Variant(Increases => Log);
+         pragma Loop_Invariant(Pow = Base**Log);
+         Pow := Mult(Pow, Base);
+         Log := Log + 1;
+      end loop;
+      return Log;
+   end Log;
+
+
+   function Mult(x, y : Positive) return Positive is
+      count : Natural := 0;
+      z : Natural := 0;
+   begin
+      while count < x and count < Integer'Last and z <= Integer'Last - y loop
+         pragma Loop_Variant(Increases => count);
+         pragma Loop_Invariant(count * y = z);
+         z := z + y;
+         count := count + 1;
+      end loop;
+      return z;
+   end Mult;
+
+end Logerithm;
---
+++ assignment5/p4/src/logerithm.ads
@@ -0,0 +1,19 @@
+package Logerithm with
+   SPARK_Mode => On
+is
+   
+   Exponent_Not_Integer : Exception;
+   
+   function Log(Base, Value : Integer) return Integer with
+     Global =>  Null,
+     Depends => (Log'Result => (Base, Value)),
+     Pre => Base > 1 and (Value = 1 or Value >= Base);
+     --  Post => Base**Log'Result <= Value and then Base**Log'Result > Value / Base;
+   
+   function Mult(x, y : Positive) return Positive with
+     Global =>  Null,
+     Depends => (Mult'Result => (x, y)),
+     Pre => x <= Integer'Last / y and y <= Integer'Last / x,
+     Post => Mult'Result = x*y;
+      
+end Logerithm;
